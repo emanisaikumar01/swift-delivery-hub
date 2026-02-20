d0b4b8cfa7b9d16dc2caa6189673116c89aac7ca
Emani Balakrishna Sai Kumar <emanisaikumar01@gmail.com>
2026-02-20 11:33:39 +0530

    Import Login component in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import Login from "./pages/Login";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
